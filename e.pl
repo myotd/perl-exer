0fcd29826e9ce5e6cd24d340b643340f0adc0fd0
myotd <github@lyse.de>
2014-10-23 15:40:16 +0200

    ende addiert

--- e.pl
+++ e.pl
@@ -21,3 +21,5 @@ print "The total of \@fred is $fred_total.\n";
 print "Enter some numbers on separate lines: ";
 my $user_total = &total(<STDIN>);
 print "The total of those numbers is $user_total.\n";
+
+# ende
